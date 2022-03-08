05dff08baf571e457646b7f95fa01c180bae1cea
michaeloye <michaeloye23@gmail.com>
2022-03-09 00:06:02 +0100

    Setup and Fetch Data from Firestore

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBS3UtDmiAZGQJy79DDPPjW5wH53bt5xi4",
@@ -12,7 +13,28 @@ const firebaseConfig = {
   messagingSenderId: "321885608856",
   appId: "1:321885608856:web:9697b7277d56b57df3e9ed",
 };
+// init firebase app
 initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books"); //database and collection name
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
