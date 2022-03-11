6f07ad58e48325877187bd1ea91936095b431fa1
michaeloye <michaeloye23@gmail.com>
2022-03-11 04:25:05 +0100

    Add update document form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-function App({ handleAdd, handleDelete }) {
+function App({ handleAdd, handleDelete, handleUpdate }) {
   const [title, setTitle] = useState();
   const [author, setAuthor] = useState();
   const [id, setId] = useState();
@@ -37,6 +37,24 @@ function App({ handleAdd, handleDelete }) {
         />
 
         <button type="submit">Delete a book</button>
+      </form>
+
+      {/* Update document form */}
+      <form className="update" onSubmit={(e) => handleUpdate(e, id, title)}>
+        <label htmlFor="id">Document Id:</label>
+        <input
+          type="text"
+          name="id"
+          required
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="text"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Update book</button>
       </form>
     </div>
   );
