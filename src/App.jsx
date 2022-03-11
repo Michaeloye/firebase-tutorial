99d47dc5257e56bb0466b2b38386a7b2b89ff88a
michaeloye <michaeloye23@gmail.com>
2022-03-11 09:49:32 +0100

    Add Signup form with necessary state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 
-function App({ handleAdd, handleDelete, handleUpdate }) {
+function App({ handleAdd, handleDelete, handleUpdate, handleSignup }) {
   const [title, setTitle] = useState();
   const [author, setAuthor] = useState();
   const [id, setId] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   return (
     <div className="App">
@@ -55,6 +57,28 @@ function App({ handleAdd, handleDelete, handleUpdate }) {
           onChange={(e) => setTitle(e.target.value)}
         />
         <button type="submit">Update book</button>
+      </form>
+
+      {/* Authentication */}
+      <h2>Firebase Auth</h2>
+
+      <form
+        className="signup"
+        onSubmit={(e) => handleSignup(e, email, password)}
+      >
+        <label htmlFor="email">email:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">password:</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">signup</button>
       </form>
     </div>
   );
